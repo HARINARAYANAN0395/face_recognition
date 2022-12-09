6b50342bd5b97e164129f612a46b3a1046c39d42
Hari Narayanan <harinarayanan@Haris-MacBook-Air.local>
2022-12-09 16:16:45 +0530

    eye_detection code

---
+++ eye_detection.py
@@ -0,0 +1,25 @@
+import cv2
+camera=cv2.VideoCapture(0)
+eye_detector=cv2.CascadeClassifier("eye_detector.xml")
+face_detector=cv2.CascadeClassifier("facemodel.xml")
+while True:
+    success,frame=camera.read()
+    grey_image=cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
+    eye_data=eye_detector.detectMultiScale(grey_image,minNeighbors=8,minSize=[30,30])
+    face_data=face_detector.detectMultiScale(grey_image,minNeighbors=8,minSize=[30,30])
+    # print(face_data)
+    for x,y,w,h in eye_data :
+        print(x,y,w,h)
+        print(len(eye_data ))
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+    for x,y,w,h in face_data :
+        print(x,y,w,h)
+        print(len(face_data ))
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        # cv2.putText(frame,"no of face detected "+str(len(face_data)),(100,200), cv2.FONT_HERSHEY_SIMPLEX , 1,(0,0,255),3)
+    cv2.imshow("live video",frame)
+    end=cv2.waitKey(1)
+    if end==ord('q'):
+            cv2.destroyAllWindows()
+            camera.release()
+            break
